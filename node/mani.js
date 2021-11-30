69a8ef94ad19f2faec1e861ecd8b541be3cbb8ac
Manishav233 <manishav233@gmail.com>
2021-12-01 02:48:57 +0530

    Initial app

---
+++ node/mani.js
@@ -0,0 +1,5 @@
+const os= require("os");
+console.log("os version",os.version());
+console.log("Free memory",os.freemem());
+console.log("total memory",os.totalmem());
+console.log("CPU",os.cpus());

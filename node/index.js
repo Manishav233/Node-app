b2c3eb42bf997b6141264595fd2932f354219fbb
Manishav233 <manishav233@gmail.com>
2021-12-01 17:17:36 +0530

    added movie by mongodb

--- node/index.js
+++ node/index.js
@@ -1,7 +1,11 @@
 // const { response } = require("express");
-const express=require("express");
+//const express=require("express");
+import express from "express";
+import {MongoClient} from "mongodb";
 const app=express();
 const PORT=9000;
+app.use(express.json());
+//express.json() -inbuilt middleware-tarnsforms body data to json, it converts every request in app
 const movies = [
   {
     id: "100",
@@ -79,37 +83,61 @@ const movies = [
     language: "English",
   },
 ];
+const MONGO_URL="mongodb://localhost";
+
+async function createConnection(){
+const client=new MongoClient(MONGO_URL);
+await client.connect();
+console.log("Mongodb Connected");
+return client;
+}
+const client= await createConnection();
+
 app.get("/",(request,response)=>{
 response.send("Hello");
 }); 
 
-app.get("/movies",(request,response)=>{
+app.get("/movies",async(request,response)=>{
     console.log(request.query);
-    const {language,rating}=request.query;
-    console.log(language,rating);
+    // const {language,rating}=request.query;
+    const filter=request.query;
+    console.log(filter);
+    if(filter.rating){
+    filter.rating=+filter.rating;
+    }
     
-    let FilterMovie=movies;
-    if(language)
-    {
-     FilterMovie=FilterMovie.filter((mv)=>mv.language===language);
-    }
-    if(rating)
-    {
-     FilterMovie=FilterMovie.filter((mv)=>mv.rating===+rating);
-    }
+    // console.log(language,rating);
+    
+    // let FilterMovie=movies;
+    // if(language)
+    // {
+    //  FilterMovie=FilterMovie.filter((mv)=>mv.language===language);
+    // }
+    // if(rating)
+    // {
+    //  FilterMovie=FilterMovie.filter((mv)=>mv.rating===+rating);
+    // }
+    const FilterMovie=await client.db("b28wd").collection("movies").find(filter).toArray();
     
 response.send(FilterMovie);
 console.log(FilterMovie);
 });
 
-app.get("/movies/:id",(request,response)=>{
+app.post("/movies",async(request,response)=>{
+const data=request.body;
+
+const result=await client.db("b28wd").collection("movies").insertMany(data);
+//console.log(result);
+response.send(result);
+});
+
+app.get("/movies/:id",async(request,response)=>{
 console.log(request.params);
 
-
-     
-     
 const {id}=request.params;
-    const movie = movies.find((mv) => mv.id===id);
+
+const movie=await client.db("b28wd").collection("movies").findOne({id:id});
+    // const movie = movies.find((mv) => mv.id===id);
      console.log(movie);
      
     movie?

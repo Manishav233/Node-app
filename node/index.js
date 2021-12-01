d2b722bc164b89d4d584cd608578371a506a232b
Manishav233 <manishav233@gmail.com>
2021-12-02 00:59:21 +0530

    changes done till route

--- node/index.js
+++ node/index.js
@@ -4,7 +4,7 @@ import express from "express";
 import {MongoClient} from "mongodb";
 import dotenv from "dotenv";
 import { getMovies, createMovies, getMovieById, deleteMovieById, updateMovieById } from "./helper.js";
-
+import {moviesRouter} from "./routes/movies.js";
 dotenv.config(); //put all keys in process.env
 console.log(process.env);
 
@@ -30,79 +30,8 @@ app.get("/",(request,response)=>{
 response.send("Hello");
 }); 
 
+app.use("/movies",moviesRouter);
 
-app.get("/movies",async(request,response)=>{
-console.log(request.query);
-// const {language,rating}=request.query;
-const filter=request.query;
-console.log(filter);
-if(filter.rating){
-filter.rating=+filter.rating;
-}
-const FilterMovie=await getMovies(filter);
-response.send(FilterMovie);
-console.log(FilterMovie);
-});
-    
-    // console.log(language,rating);
-    
-    // let FilterMovie=movies;
-    // if(language)
-    // {
-    //  FilterMovie=FilterMovie.filter((mv)=>mv.language===language);
-    // }
-    // if(rating)
-    // {
-    //  FilterMovie=FilterMovie.filter((mv)=>mv.rating===+rating);
-    // }
-    
-
-
-
-app.post("/movies",async(request,response)=>{
-const data=request.body;
-const result=await createMovies(data);
-//console.log(result);
-response.send(result);
-});
-
-app.get("/movies/:id",async(request,response)=>{
-console.log(request.params);
-const {id}=request.params;
-const movie=await getMovieById(id);
-// const movie = movies.find((mv) => mv.id===id);
-console.log(movie);
-movie?
-response.send(movie)
-: response.status(404).send({message:"No matching movie found"});
-});
-       
-       
-app.delete("/movies/:id",async(request,response)=>{
-console.log(request.params);
-const {id}=request.params;
-const result=await deleteMovieById(id);
-// const movie = movies.find((mv) => mv.id===id);
-console.log(result);
-console.log(result.deletedCount);
-result.deletedCount>0
-?response.send(result)
-:response.status(404).send({message:"No matching movie found"});
-});
-               
-               
-app.put("/movies/:id",async(request,response)=>{
-console.log(request.params);
-const {id}=request.params;
-const data=request.body;
-const result=await updateMovieById(id, data);
-const movie=await getMovieById(id);
-// const movie = movies.find((mv) => mv.id===id);
-//console.log(result);
-response.send(movie);               
-});
-     
-     
 app.listen(PORT,()=>console.log("app started at",PORT));
 
 
---
+++ node/routes/movies.js
@@ -0,0 +1,84 @@
+import express from "express";
+const router=express.Router();
+
+import{
+    updateMovieById,
+    createMovies,
+    getMovies,
+    deleteMovieById,
+    getMovieById
+  } from "../helper.js";
+
+// .. because we need to go one folder up to ftech data from helper.js
+router.get("/",async(request,response)=>{
+    console.log(request.query);
+    // const {language,rating}=request.query;
+    const filter=request.query;
+    console.log(filter);
+    if(filter.rating){
+    filter.rating=+filter.rating;
+    }
+    const FilterMovie=await getMovies(filter);
+    response.send(FilterMovie);
+    console.log(FilterMovie);
+    });
+        //we change app.get......... to router.get.....
+        // console.log(language,rating);
+        
+        // let FilterMovie=movies;
+        // if(language)
+        // {
+        //  FilterMovie=FilterMovie.filter((mv)=>mv.language===language);
+        // }
+        // if(rating)
+        // {
+        //  FilterMovie=FilterMovie.filter((mv)=>mv.rating===+rating);
+        // }
+        
+    
+    
+    
+    router.post("/",async(request,response)=>{
+    const data=request.body;
+    const result=await createMovies(data);
+    //console.log(result);
+    response.send(result);
+    });
+    
+    router.get("/:id",async(request,response)=>{
+    console.log(request.params);
+    const {id}=request.params;
+    const movie=await getMovieById(id);
+    // const movie = movies.find((mv) => mv.id===id);
+    console.log(movie);
+    movie?
+    response.send(movie)
+    : response.status(404).send({message:"No matching movie found"});
+    });
+           
+           
+    router.delete("/:id",async(request,response)=>{
+    console.log(request.params);
+    const {id}=request.params;
+    const result=await deleteMovieById(id);
+    // const movie = movies.find((mv) => mv.id===id);
+    console.log(result);
+    console.log(result.deletedCount);
+    result.deletedCount>0
+    ?response.send(result)
+    :response.status(404).send({message:"No matching movie found"});
+    });
+                   
+                   
+    router.put("/:id",async(request,response)=>{
+    console.log(request.params);
+    const {id}=request.params;
+    const data=request.body;
+    const result=await updateMovieById(id, data);
+    const movie=await getMovieById(id);
+    // const movie = movies.find((mv) => mv.id===id);
+    //console.log(result);
+    response.send(movie);               
+    });
+    
+    export const moviesRouter=router;
